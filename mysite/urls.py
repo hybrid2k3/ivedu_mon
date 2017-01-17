67ddd61f6c3398a2f16c3f144c6b9bcc551130ae
hybrid2k3 <hybr@mail.ru>
2017-01-17 11:00:58 +0300

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.conf.urls import include
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('ivedu_mon.urls')),
+#    url(r'blog/', include('ivedu_mon.urls')),
+
 ]
